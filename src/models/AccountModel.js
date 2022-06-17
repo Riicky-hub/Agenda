afeb9fe8e728b15052c86b02914bab732e80ac2c
riicky-hub <richardjosephace1960@gmail.com>
2022-06-16 21:37:21 -0300

    melhorando validacao e adicionando criptografia na senha

--- src/models/AccountModel.js
+++ src/models/AccountModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const AccountSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -17,6 +18,12 @@ class Account {
   async register() {
     this.valid();
     if(this.errors.length > 0) return;
+    await this.userExits();
+    if(this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
     try {
       this.user = await AccountModel.create(this.body);
     } catch(e) {
@@ -32,6 +39,9 @@ class Account {
     if(this.body.password.length <= 3 || this.body.password.length >= 30) {
       this.errors.push('A senha precisa ter entre 3 e 30 caracteres');
     }
+    if(this.body.confirm !== this.body.password) {
+      this.errors.push('As senhas precisam ser as mesmas');
+    }
   }
   cleanUp() {
     for(const key in this.body) {
@@ -41,8 +51,13 @@ class Account {
     }
     this.body = {
       email: this.body.email_cadastro,
-      password: this.body.password_cadastro
+      password: this.body.password_cadastro,
+      confirm: this.body.confirm_cadastro
     }
+  }
+  async userExits() {
+    const user = await AccountModel.findOne({ email: this.body.email });
+    if(user) this.errors.push('Usuário já existe!');
   }
 }
 
